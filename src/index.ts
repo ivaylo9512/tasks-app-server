ac1ff830ebf1bf79abd49944fcf352035b96f4ef
ivaylo <ivayloaleksandrov@yahoo.com>
2021-06-26 23:55:48 +0300

    setting MikroOrm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hey")
+import { MikroORM } from "@mikro-orm/core"
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'tasks-app',
+        entities: [],
+        user: 'postgres',
+        password: '1234',
+        debug: process.env.NODE_ENV !== 'production',
+        type: 'postgresql'
+    });
+}
+
+main();
