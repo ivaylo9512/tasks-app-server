e1f1e9a4376398a2ae98ba6dc4d711d2b6149478
ivaylo <ivayloaleksandrov@yahoo.com>
2021-06-26 23:57:14 +0300

    Post entity added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { MikroORM } from "@mikro-orm/core"
+import { Post } from "./entities/Post";
 
 const main = async () => {
     const orm = await MikroORM.init({
         dbName: 'tasks-app',
-        entities: [],
+        entities: [Post],
         user: 'postgres',
         password: '1234',
         debug: process.env.NODE_ENV !== 'production',
