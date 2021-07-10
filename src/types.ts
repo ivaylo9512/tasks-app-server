291d382a84bd30590d37ebba7967e1e258a56388
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-10 23:18:19 +0300

    types small fix TaskInput to InputType + apolloContext services added

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,27 @@
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import {Request, Response} from 'express'
 import TaskService from "./service/task-service-impl";
 import UserService from "./service/base/user-service";
+import { InputType, Field } from 'type-graphql';
+
 export type ApolloContext = {
-    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>,
+    services: {
+        taskService: TaskService,
+        userService: UserService
+    },
     req: Request,
     res: Response
 }
-export type TaskInput = {
-    id?: number,
-    from: Date,
-    to: Date,
-    daily: boolean,
+@InputType()
+export class TaskInput {
+    @Field()
+    id?: number
+    @Field()
+    from: Date
+    @Field()
+    to: Date
+    @Field()
+    daily: boolean
+    @Field()
     alertAt: Date
 }
 export interface TaskRequest extends Request {
