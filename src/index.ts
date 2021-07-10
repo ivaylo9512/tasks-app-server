efae362456dc4f30467d172b595af3bee5a29e4e
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-10 23:55:49 +0300

    app set error handle + RequestContext + routers + services

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import 'reflect-metadata';
-import { MikroORM } from '@mikro-orm/core';
+import { MikroORM, RequestContext } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { TaskResolver } from './resolvers/task';
-import { ApolloContext } from './types';
+import { ApolloContext, TaskRequest, UserRequest } from './types';
 import cors from 'cors';
+import TaskService from './service/task-service-impl';
+import UserService from './service/user-service-impl';
+import TaskRouter from './routers/task-routes';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
+
+    const taskService = new TaskService(orm.em);
+    const userService = new UserService(orm.em);
 
     const app = express();
     app.use(cors({
@@ -18,12 +24,44 @@ const main = async () => {
         credentials: true
     }))
     
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use((req, res, next) => {
+        RequestContext.create(orm.em, next);
+    });
+      
+    app.use('/tasks', (req: TaskRequest, res, next) => {
+        req.service = taskService;
+        next();
+    }, TaskRouter);
+    
+    app.use('/users', (req: UserRequest, res, next) => {
+        req.service = userService;
+        next();
+    },);
+
+    app.use((err, req, res, next) => {
+        if(err.message.includes('not found')){
+            res.status(404).send('Entity not found!');
+        }else{
+            err.status(500).send(err.message)
+        }
+      })
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [TaskResolver],
             validate: false
         }),
-        context: ({req, res}): ApolloContext => ({ em: orm.em, req, res })
+        context: ({req, res}): ApolloContext => ({ 
+            services: {
+                userService, 
+                taskService
+            }, 
+            req, 
+            res
+        })
     });
 
     apolloServer.applyMiddleware({ app })
