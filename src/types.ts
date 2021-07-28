c70833843fdf2f5a8f2e12d8f404af22895e2f01
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-28 17:54:13 +0300

    Requests + User removed from types + graphql-middleware package added

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,14 @@
 import {Request, Response} from 'express'
 import TaskService from "./service/task-service-impl";
 import UserService from "./service/base/user-service";
-import { UserInput } from './resolvers/types/user-input';
+import UserEntity from './entities/user';
 
 export type ApolloContext = {
-    services: {
-        taskService: TaskService,
-        userService: UserService
-    },
+    userService: UserService;
+    taskService: TaskService
+    foundUser?: UserEntity;   
     req: Request,
     res: Response
-}
-export type LoggedUser = {
-    id: number,
-    role: string
-}
-export interface TaskRequest extends Request {
-    service?: TaskService
-}
-export interface UserRequest extends Request {
-    service?: UserService
-    body: UserInput
-}
-declare global {
-    namespace Express {
-      interface User extends LoggedUser{
-      }
-    }
 }
 declare module 'jsonwebtoken' {
     function verify(token: string, secretOrPublicKey: Secret, options?: VerifyOptions): LoggedUser;
