b7921a4cb8b59e7ffe7644563dd8a79b27f16952
ivaylo <ivayloaleksandrov@yahoo.com>
2021-07-28 23:57:32 +0300

    auth middleware added

---
+++ src/resolvers/middlewares/auth.ts
@@ -0,0 +1,45 @@
+import { IMiddlewareFunction } from "graphql-middleware";
+import UnauthorizedException from "../../exceptions/unauthorized";
+import { Request } from 'express';
+import { verify } from "jsonwebtoken";
+import { jwtSecret } from "../../utils/jwt";
+import { ApolloContext } from "../../types";
+
+type AuthMiddleware = {
+    [name: string]: {
+        [name: string]: IMiddlewareFunction
+    }
+}
+
+const authenticate = (async (resolve, parent, args, context: ApolloContext, info) => {
+    const jwtUser = getUserFromToken(context.req, context)
+    context.foundUser = await context.userService.verifyLoggedUser(jwtUser.id);
+ 
+    return await resolve(parent, args, context, info)
+}) as IMiddlewareFunction
+
+const authMiddleware: AuthMiddleware = {
+    Mutation: {
+        deleteTask: authenticate,
+        createTask: authenticate,
+    },
+    Query: {
+        tasksByDate: authenticate,
+        tasksByState: authenticate,
+    }
+}
+
+const getUserFromToken = (req: Request, context: ApolloContext) => {
+    let token = req.headers?.authorization;
+    if(!token){
+        throw new UnauthorizedException('No auth token');
+    }
+    token = token.split(' ')[1];
+
+    const jwtUser = verify(token, jwtSecret);
+    context.jwtUser = jwtUser;
+
+    return jwtUser;
+}
+
+export default authMiddleware
